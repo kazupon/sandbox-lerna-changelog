32943a74f38c1d0f43f9fdf5a8c5fc773070264d
kazuya kawaguchi <kawakazu80@gmail.com>
2019-11-01 18:53:13 +0900

    fix: beforeCommitChanges calling (#7)

--- ship.config.js
+++ ship.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   updateChangelog: false,
   beforeCommitChanges: ({ exec, dir }) => {
-    console.log('---beforeCommitChanges', dir)
-    console.log('------')
-    exec(`npx lerna-changelog`)
+    return Promise.resolve(() => {
+      console.log('---beforeCommitChanges', dir)
+      console.log('------')
+      exec(`npx lerna-changelog`)
+    });
   },
   publishCommand: () => 'echo dummy publish!'
 }
