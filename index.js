6e67c10588b0cab743c542f6a9b10b4e2e07eeb2
kazuya kawaguchi <kawakazu80@gmail.com>
2019-11-24 18:20:45 +0900

    feat: semver

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello, world!')
+const { diff } = require('semver')
+
+const items = [
+  ['0.0.2', '0.0.1']
+]
+items.forEach(function (item) {
+  console.log(item)
+  const v1 = item[0]
+  const v2 = item[1]
+  console.log(v1, v2, diff(v1, v2))
+})
